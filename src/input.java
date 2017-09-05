1113b9dcdddb939f9998d653519c3c637a0bef6d
Phung Ngo <pngo@phungs-mbp.nvstu.edu>
2017-09-05 17:01:26 -0400

    Initial commit

---
+++ src/input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class input {
+
+
+        public static void main(String[] args) {
+
+            String username, password;
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter your username");
+            username = keyboard.nextLine();
+            System.out.println("Enter your password");
+            password = keyboard.nextLine();
+            System.out.println("Hello " + username + ", Welcome to CSC200 class! and Your password is " + password);
+            keyboard.close();
+
+
+        }
+
+    }
+}
